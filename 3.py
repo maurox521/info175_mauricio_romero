293b3a775c3a6e3d538ce42ba2d88f1d9175e4f9
mauri521 <maurox247@gmail.com>
2016-04-01 00:05:20 -0300

    subido ejercicio 3.3

---
+++ 3.py
@@ -0,0 +1,20 @@
+class persona():
+    def __init__(self, rut, nombre, fecha ,genero):
+        self.rut= rut
+        self.nombre= nombre
+        self.fecha = fecha
+        self.genero = genero
+    #def obtener edad(self):
+        
+class nota():
+     def __init__(self, valor, ponderacion, ramo ,carrera):
+        self.valor= valor
+        self.ponderacion = ponderacion
+        self.ramo = ramo
+        self.carrera=carrera
+     #def obtener_valor(self):
+     #def obtener_ponderacion(self):
+     #def modificar(self):
+#class alumno(persona):
+     #def __init__(self, valor, ponderacion, ramo ,carrera):
+    
